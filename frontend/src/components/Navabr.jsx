a2ec8daa8e9112f09d2ae8d217d53d3030cffb7e
ollvishnu <129575673+ollvishnu@users.noreply.github.com>
2024-10-05 18:07:49 +0530

    logo changes implemented

--- frontend/src/components/Navabr.jsx
+++ frontend/src/components/Navabr.jsx
@@ -1,10 +1,14 @@
+import { px } from 'framer-motion';
 import React from 'react';
 
 const Navbar = () => {
   return (
     <nav className="bg-blue-600 text-white p-4 shadow-md">
       <div className="container mx-auto flex justify-between">
-        <h1 className="text-xl font-bold">School Review Platform</h1>
+      <div className="flex justify-center mb-6">
+        <img src="" alt="Logo" className="w-32 h-auto" />
+      </div>
+        <h1 className="text-xl font-bold">Student Says</h1>
         <ul className="flex space-x-4">
           <li><a href="/" className="hover:underline">Home</a></li>
         </ul>
